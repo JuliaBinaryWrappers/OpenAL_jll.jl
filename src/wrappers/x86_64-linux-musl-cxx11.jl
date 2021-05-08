974c6e7eeba2518e894d5b9355436be666336956
jlbuild <juliabuildbot@gmail.com>
2021-05-08 17:09:58 +0000

    OpenAL_jll build 1.21.1+0

--- src/wrappers/x86_64-linux-musl-cxx11.jl
+++ src/wrappers/x86_64-linux-musl-cxx11.jl
@@ -4,10 +4,11 @@ export libopenal
 using alsa_jll
 using Ogg_jll
 using CompilerSupportLibraries_jll
+using PulseAudio_jll
 JLLWrappers.@generate_wrapper_header("OpenAL")
 JLLWrappers.@declare_library_product(libopenal, "libopenal.so.1")
 function __init__()
-    JLLWrappers.@generate_init_header(alsa_jll, Ogg_jll, CompilerSupportLibraries_jll)
+    JLLWrappers.@generate_init_header(alsa_jll, Ogg_jll, CompilerSupportLibraries_jll, PulseAudio_jll)
     JLLWrappers.@init_library_product(
         libopenal,
         "lib/libopenal.so",
